ff6dcaecce7455d355c12ebdd4c08ec637474672
Athul <athul2ff@gmail.com>
2021-08-07 07:28:10 +0530

    version

--- action.js
+++ action.js
@@ -2,10 +2,33 @@ $(document).ready(function(){
     $("#btn-cmt").click(function(){
       $("#box-form").toggle(1000)
     }),
+     
 
   
-    $("#submit-form").submit((e)=>{
-        e.preventDefault()
+    $("#submit-form").validate({
+        rules:{
+            NAME:{
+              required :true,
+              minlength:3
+             
+                
+            },
+            PHONE:{
+                required:true,
+                minlength:10,
+                maxlength:10
+            },
+            MAIL:{
+                required:true,
+                email:true,
+            },
+            COMMENTS:{
+                required:true
+            },
+            
+        },
+        submitHandler : function(form){
+
         $.ajax({
             url:"https://script.google.com/macros/s/AKfycbyzAi2eA-IflphFe5MW1p9T4dfbatrXUo7arOhUsyzLbZ9s6WOkLsX_vYYJL40n9N2g/exec",
             
@@ -21,6 +44,7 @@ $(document).ready(function(){
 
             }
         })
+     }
     })
 
 
